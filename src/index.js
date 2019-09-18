834c3f1876bc611eaa73ddbdaf75d75438e5d7e4
Christina Oberlin <christina.oberlin@gmail.com>
2019-09-18 18:14:37 -0400

    Use JSX

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDom from 'react-dom';
 
 const tasks = ['take out the trash', 'go for a walk'];
 
-const element = React.createElement('ol', null, tasks.map((task, index) =>  React.createElement('li', {key: index}, task)))
+const element = <ol> { tasks.map((task, index) => <li key={index}>{task}</li>) }
+</ol>
+
 ReactDom.render(element, document.getElementById('root'));
