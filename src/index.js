757003d6c238637abc4f689bfbc9cefbb59f5312
Christina Oberlin <christina.oberlin@gmail.com>
2019-11-12 11:07:21 -0500

    Use components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
 const tasks = ['take out the trash', 'go for a walk'];
 
-const element =
-<div>
-  <h1> Task List </h1>
-	<ol> { tasks.map((task, index) => <li key={index}>{task}</li>) }</ol>
-</div>
+class List extends Component {
+  render() {
+    return <ol>
+             {tasks.map((task, index) => <li key={index}>{task}</li>)}
+           </ol>;
+  }
+}
 
-ReactDom.render(element, document.getElementById('root'));
+class Title extends Component {
+  render() {
+    return <h1> Task List </h1>;
+  }
+}
+
+class Main extends Component {
+  render() {
+    return <div>
+             <Title />
+             <List />
+             <List />
+             <List />
+           </div>;
+  }
+}
+
+ReactDom.render(<Main />, document.getElementById('root'));
