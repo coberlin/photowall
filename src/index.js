b11073b05ac5f7b947547444d93f22c513277df8
Christina Oberlin <christina.oberlin@gmail.com>
2019-11-12 10:48:00 -0500

    Add title

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDom from 'react-dom';
 
 const tasks = ['take out the trash', 'go for a walk'];
 
-const element = <ol> { tasks.map((task, index) => <li key={index}>{task}</li>) }
-</ol>
+const element =
+<div>
+  <h1> Task List </h1>
+	<ol> { tasks.map((task, index) => <li key={index}>{task}</li>) }</ol>
+</div>
 
 ReactDom.render(element, document.getElementById('root'));
