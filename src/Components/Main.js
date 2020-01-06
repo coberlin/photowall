1a28e52aac5b322b49ebe8d84f6069a30ce2efd0
Christina Oberlin <christina.oberlin@gmail.com>
2020-01-06 10:58:07 -0500

    Hook up remove button

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -22,11 +22,20 @@ class Main extends Component {
 								 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
 							}],
 		};
+		this.removePhoto = this.removePhoto.bind(this);
 	}
+
+	removePhoto(postRemoved) {
+    console.log(postRemoved.description);
+    this.setState((state) => ({
+      posts: state.posts.filter(post => post !== postRemoved)
+    }));
+	}
+
   render() {
     return <div>
              <Title title={'Photowall'}/>
-             <Photowall posts={this.state.posts}/>
+             <Photowall posts={this.state.posts} onRemovePhoto={this.removePhoto}/>
            </div>;
   }
 }
--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -3,7 +3,7 @@ import Photo from './Photo';
 
 function PhotoWall(props) {
 	return <div className="photoGrid">
-		       {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+		       {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
 				 </div>
 }
 
